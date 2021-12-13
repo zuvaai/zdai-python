b13e55a765051d5bcfa0bea6382994385ccc3dcb
flongtin <francois.longtin@kirasystems.com>
2021-12-13 14:07:39 -0500

    New sample class to create new custom fields

---
+++ zdai/CustomFieldTrainer.py
@@ -0,0 +1,103 @@
+from zdai import ZDAISDK
+import time
+
+
+class CustomFieldTrainer:
+    """
+    The CustomFieldTrainer class can be used to create new custom fields using the DocAI
+    Training APIs.
+
+    Example usage:
+
+        custom_field = CustomFieldTrainer(sdk = ZDAISDK(from_config = True), name = 'customfieldname')
+        custom_field.create_empty()
+        custom_field.add_annotation(file_id = 'abc', start = 100, end = 150)
+        custom_field.add_annotation(file_id = 'abc', start = 500, end = 550)
+        custom_field.train()
+        print(custom_field.get_accuracy())
+    """
+    def __init__(self, sdk: ZDAISDK, name: str, field_id: str = None):
+        self.name = name
+        self._sdk = sdk
+        self.description = None
+        self.field_id = field_id
+        self.annotations = []
+
+    def sdk(self):
+        """
+        An instance of the ZDAISDK that was passed through the CustomFieldTrainer constructor
+        Returns as a method
+        """
+        return self._sdk
+
+    def create_empty(self):
+        """
+        Creates a new custom field
+        """
+        field_id, _ = self.sdk().fields.create(field_name = self.name)
+        self.field_id = field_id
+
+    def create_from_field(self, field_id):
+        """
+        Creates a new custom field using another field as its base
+        """
+        field_id, _ = self.sdk().fields.create(field_name = self.name,
+                                        from_field_id = field_id)
+        self.field_id = field_id
+
+    def add_annotation(self, file_id: str, start: int, end: int):
+        """
+        Add a start and end character range that belongs to a file_id.
+        This range will be used during training.
+        """
+        def exists(file_id):
+            return any([a for a in self.annotations if a.get('file_id') == file_id])
+
+        def add(file_id: str, start: int, end: int):
+            if not exists(file_id):
+                self.annotations.append({
+                    "file_id": file_id,
+                    "locations": [
+                        {
+                            "start": start,
+                            "end": end
+                        }
+                    ]
+                })
+            else:
+                for annotation in self.annotations:
+                    if annotation.get('file_id') == file_id:
+                        annotation.get('locations').append({
+                            "start": start,
+                            "end": end
+                        })
+
+        add(file_id, start, end)
+
+    def train(self):
+        """
+        Creates a new training request using the annotations
+        """
+        request, _ = self.sdk().fields.train(field_id = self.field_id, annotations = self.annotations)
+
+        while True:
+            latest = request.update()
+            print(f'{latest.type} is {latest.status}')
+
+            if request.is_finished():
+                break
+
+            time.sleep(2)
+
+    def get_accuracy(self):
+        """
+        Get the field's accuracy
+        """
+        accuracy, _ = self.sdk().fields.get_accuracy(field_id = self.field_id)
+        return accuracy
+
+    def get_layout(self):
+        """
+        Get the custom field's protobuf layout
+        """
+        return self.sdk().fields.get_layout(field_id = self.field_id)
