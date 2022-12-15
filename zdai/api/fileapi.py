0cb01c1fe46313d898acea23581578ff35c1e71a
flongtin <francois@zuva.ai>
2022-12-15 15:49:57 -0500

    Updated `create` so that users can set the file's expiration date during file submission.
    If `is_zuva_ocr` is provided then the `Content-Type` will be set to the new `application/eocr` rather than the previous `application/kiraocr`
    
    The `delete` function's `file_id` `Typing` is now `str` from `id` (since DocAI `file_id` are `xid` and not integers.
    
    New `set_expiration` method which accepts `file_id` and `expiration` to extend a `file_id`'s expiration date.

--- zdai/api/fileapi.py
+++ zdai/api/fileapi.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 from ..api.apicall import ApiCall
-from ..models.file import File
+from ..models.file import File, FileExpiration
 from typing import Tuple
 from datetime import datetime
 
@@ -26,18 +26,21 @@ class FileAPI(object):
     def __init__(self, token: str, url: str):
         self._call = ApiCall(token, url)
 
-    def create(self, content: bytes, is_zuva_ocr: bool = False) -> Tuple[File, ApiCall]:
+    def create(self, content: bytes, is_zuva_ocr: bool = False, expiration: str = None) -> Tuple[File, ApiCall]:
         """
         Creates a new file in the ZDAI
 
         :param content: The byte-content of the data to submit.
         :param is_zuva_ocr: If the byte content provided comes from a .zuvaocr file.
+        :param expiration: Set the expiration of the document. Defaults to 7d in DocAI. Max 13d.
         :return:
         """
         caller = self._call.new(method = 'POST', path = f'files')
         caller.use_default_accept_type = False
         caller.use_default_content_type = False
-        if is_zuva_ocr: caller.add_header(key = 'Content-Type', value = 'application/kiraocr')
+        if is_zuva_ocr: caller.add_header(key = 'Content-Type', value = 'application/eocr')
+        if expiration: caller.add_header(key = 'Expiration', value = expiration)
+
         caller.set_body_value(value = content)
         caller.send()
 
@@ -49,10 +52,9 @@ class FileAPI(object):
             expiration = datetime.strptime(data.get('expiration'), '%Y-%m-%dT%H:%M:%SZ')
         )
 
-
         return file, caller
 
-    def delete(self, file_id: int) -> Tuple[bool, ApiCall]:
+    def delete(self, file_id: str) -> Tuple[bool, ApiCall]:
         """
         Deletes a file
 
@@ -62,3 +64,30 @@ class FileAPI(object):
         caller.send()
 
         return caller.response.status_code == 204, caller
+
+    def set_expiration(self, file_id: str, expiration: str):
+        """
+        Sets the file expiration
+
+        Example expirations are:
+            2022-12-17T00:00:00Z — sets the expiration to December 17, 2022
+            1d — sets the expiration to 1 day from when this call is run
+            13d — sets the expiration to 13 days from when this all is run
+
+        :param file_id: The file ID for which the file expiration will be set
+        :param expiration: The new expiration (formatted YYYY-MM-DDTHH:mm:ssZ), or length of time (e.g. 7d for 7 days),
+        :return: The file_id updated and the new expiration date
+        """
+
+        caller = self._call.new(method = 'PUT', path = f'files/{file_id}/expiration')
+        caller.add_header(key = 'Expiration', value = expiration)
+        caller.send()
+
+        new_expiration = caller.response.json().get('expiration')
+        if not new_expiration:
+            raise Exception(f'No expiration found for file_id {file_id}.')
+
+        new_expiration = datetime.strptime(new_expiration, "%Y-%m-%dT%H:%M:%SZ")
+
+        return FileExpiration(id = caller.response.json().get('file_id'),
+                              expiration = new_expiration), caller
