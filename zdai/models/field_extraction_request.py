322f4d2a835bb274580f9891cb4dec099acb9422
hannah <hannah@zuva.ai>
2023-08-28 10:23:35 -0400

    format with autopep8

--- zdai/models/field_extraction_request.py
+++ zdai/models/field_extraction_request.py
@@ -19,10 +19,11 @@ class FieldExtractionRequest(BaseRequest):
     """
     The class used for requests created in the Field Extraction service
     """
+
     def __init__(self, api, json):
-        super().__init__(api = api, json = json)
+        super().__init__(api=api, json=json)
 
     def get_results(self):
-        data, _ = self.api().get_result(request_id = self.id)
+        data, _ = self.api().get_result(request_id=self.id)
 
         return data
